7a5f0ad4a20a46302af09f737c7d457dad29dce6
buffard <samuelwebber19@gmail.com>
2019-08-20 15:08:39 -0500

    working on the post call

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -3,7 +3,16 @@ const API = {
   getJournalEntries () {
     return fetch(URL)
       .then(entries => entries.json())
-  }
+  },
+  postJournalEntries (entry) {
+    return fetch(URL, { 
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json"
+       },
+       body: JSON.stringify(entry)
+   })
+   }
 }
 
 export default API;
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,5 +3,22 @@ import insertEntry from "./entriesDOM.js"
 
 API.getJournalEntries().then(data => insertEntry.insertHtml(data))
 
+let postDate = document.querySelector("#postDate").value
+let concepts = document.querySelector("#concepts").value
+let entry = document.querySelector("#entry")
+let mood = document.querySelector("#mood")
+let submitBtn = document.querySelector("#submit")
+
+submitBtn.addEventListener('click', () => {
+  
+  console.log(concepts)
+})
+
+const createEntry = (postDate) => {
+  return{
+    postDate: postDate.postDate
+  }
+}
+
 
 
